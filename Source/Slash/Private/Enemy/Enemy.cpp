ca2f8b572e6dfc246e59305d3ccfa876e1cba255
Guillaume Norgol <52493523+gnorgol@users.noreply.github.com>
2023-06-27 15:02:06 -0400

    Add new Macro DRAW_SPHERE_COLOR

--- Source/Slash/Private/Enemy/Enemy.cpp
+++ Source/Slash/Private/Enemy/Enemy.cpp
@@ -45,6 +45,6 @@ void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void AEnemy::GetHit(const FVector& ImpactPoint)
 {
-	DRAW_SPHERE(ImpactPoint);
+	DRAW_SPHERE_COLOR(ImpactPoint, FColor::Orange);
 }
 
