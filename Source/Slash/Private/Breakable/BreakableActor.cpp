2d9c6d5bbb827e9dc846c44d1b920c1ac695c3e6
Guillaume Norgol <52493523+gnorgol@users.noreply.github.com>
2023-06-30 08:32:26 -0400

    Add Spawn Actor Treasure

--- Source/Slash/Private/Breakable/BreakableActor.cpp
+++ Source/Slash/Private/Breakable/BreakableActor.cpp
@@ -2,8 +2,9 @@
 
 
 #include "Breakable/BreakableActor.h"
-
+#include "Components/CapsuleComponent.h"
 #include "GeometryCollection/GeometryCollectionComponent.h"
+#include "Items/Treasure.h"
 
 
 ABreakableActor::ABreakableActor()
@@ -15,6 +16,13 @@ ABreakableActor::ABreakableActor()
 	SetRootComponent(GeometryCollection);
 	GeometryCollection->SetGenerateOverlapEvents(true);
 	GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	GeometryCollection->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	Capsule->SetupAttachment(GetRootComponent());
+	Capsule->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	Capsule->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Block);
+
 
 
 }
@@ -34,7 +42,12 @@ void ABreakableActor::Tick(float DeltaTime)
 
 void ABreakableActor::GetHit_Implementation(const FVector& ImpactLocation)
 {
-
-
+	UWorld* World = GetWorld();
+	if (World && TreasureClass)
+	{
+		FVector SpawnLocation = GetActorLocation();
+		SpawnLocation.Z += 75.f;
+		World->SpawnActor<ATreasure>(TreasureClass, SpawnLocation, FRotator::ZeroRotator);
+	}
 }
 
--- Source/Slash/Public/Breakable/BreakableActor.h
+++ Source/Slash/Public/Breakable/BreakableActor.h
@@ -25,10 +25,18 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		UGeometryCollectionComponent* GeometryCollection;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UCapsuleComponent* Capsule;
+
 private:
 
-	UPROPERTY(VisibleAnywhere)
-	UGeometryCollectionComponent* GeometryCollection;
+
+
+	UPROPERTY(EditAnywhere, Category = "Breakable Properties")
+		TSubclassOf<class ATreasure> TreasureClass;
 
 	
 
